99e22b21e184cd56533bf07f5739c4a65e27b61a
翁坚铨 <2410808055@qq.com>
2018-04-01 18:46:28 +0800

    Update 006-ZigZag-Conversion.js

--- 006-ZigZag-Conversion.js
+++ 006-ZigZag-Conversion.js
@@ -15,3 +15,34 @@
  * convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
  *
  */
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+
+var convert = function (s, numRows) {
+    var len = s.length;  
+    var d = 2 * numRows - 2; // 两整列之间的下标差
+    var result = '';  
+  
+    if (len == 0 || numRows == 0 || numRows == 1) return s;  
+         
+    // 从第一行遍历到最后一行     
+    for (var i = 0; i < numRows; i++) {
+        for (var j = i; j < len; j += d) {  
+            var singleCharColumn =  j + d - 2 * i ;
+
+            result += s.charAt(j); // 非单个字符列的字符  
+
+            if (i != 0 && i != numRows - 1 &&  singleCharColumn < len) {
+                result += s.charAt(singleCharColumn); // 单个字符列的字符  
+            } 
+        }  
+    }   
+
+    return result ;  
+};
+console.log(convert('PAYPALISHIRING', 3)); // PAHNAPLSIIGYIR
+console.log(convert('ABCDEFGHIjKLMNO', 5)); // AIBHjCGKODFLNEM
