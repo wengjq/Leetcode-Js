28368de2e9b83cc2bc2700e663081c0e76415e65
翁坚铨 <2410808055@qq.com>
2018-04-06 09:01:50 +0800

    Update 007-Reverse-Integer.js

--- 007-Reverse-Integer.js
+++ 007-Reverse-Integer.js
@@ -17,3 +17,31 @@
  * Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range.
  * For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
  */
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+
+    var sign = x >= 0 ? 1 : -1;
+    x = Math.abs(x);
+
+    var sum = 0;
+
+    while (x) {
+        sum = sum * 10 + x % 10;
+        x = Math.floor(x / 10);
+    }
+
+    var ret = sign * sum;
+    var max = Math.pow(2, 31) - 1;
+    var min = -Math.pow(2, 31);
+
+    if (ret > max) return 0;
+    if (ret < min) return 0;
+
+    return ret;
+};
+
+console.log(reverse(-123)); // -321
