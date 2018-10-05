3cd9a6b0d85a23c97a5e16505324d32cdc1561a0
wengjq <2410808055@qq.com>
2018-10-05 16:46:43 +0800

    Create 049-group-anagrams.js

---
+++ 049-group-anagrams.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  var ans = [];
+  var hash = {};
+
+  strs.forEach(function(item) {
+    var str = item.split('').sort().join('');
+    if (hash[str] === undefined) {
+      hash[str] = ans.length;
+      ans.push([item]);
+    } else {
+      ans[hash[str]].push(item);
+    }
+  });
+
+  return ans;
+};
