416c233937f7ceddcbe5257bb8b845d6e180edc3
wengjq <2410808055@qq.com>
2019-02-11 17:09:26 +0800

    Update 053-Maximum-Subarray.js

--- 053-Maximum-Subarray.js
+++ 053-Maximum-Subarray.js
@@ -25,3 +25,35 @@ var maxSubArray = function (nums) {
 
     return max;
 };
+
+
+// 分治法，把数组一分为二，分别找出左边和右边的最大子数组之和，然后还要从中间开始向左右分别扫描，求出的最大值分别和左右两边得出的最大值相比较取最大的那一个个
+var maxSubArray = function (nums) {
+	if (!nums.length) return 0;
+
+	return helper(nums, 0, nums.length - 1);
+}
+
+function helper(nums, left, right) {
+	if (left >= right) return nums[left];
+
+    var mid = Math.floor(left + (right - left) / 2);
+    var lmax = helper(nums, left, mid - 1);
+    var rmax = helper(nums, mid + 1, right);
+    var mmax = nums[mid];
+    var t = mmax;
+
+    for (var i = mid - 1; i >= left; --i) {
+        t += nums[i];
+        mmax = Math.max(mmax, t);
+    }
+
+    t = mmax;
+
+    for (var i = mid + 1; i <= right; ++i) {
+        t += nums[i];
+        mmax = Math.max(mmax, t);
+    }
+
+    return Math.max(mmax, Math.max(lmax, rmax));
+}
