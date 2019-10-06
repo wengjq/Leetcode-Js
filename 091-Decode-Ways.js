336a8434b9ca52527612d0712e08015edca1b4db
wengjq <2410808055@qq.com>
2019-10-06 23:18:48 +0800

    Update 091-Decode-Ways.js

--- 091-Decode-Ways.js
+++ 091-Decode-Ways.js
@@ -9,7 +9,9 @@ var numDecodings = function(s) {
   dp[0] = 1;
 
   for (var i = 1; i < s.length + 1; ++i) {
+	// 每个数字判断是否为0
     dp[i] = (s[i - 1] == '0') ? 0 : dp[i - 1];
+	// 跟前一个数字组成大于等于 10 且小于等于 26 的数
     if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
       dp[i] += dp[i - 2];
     }
