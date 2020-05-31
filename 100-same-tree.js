58f2f248cd4ab195804168668a662fefde460af6
wengjq <2410808055@qq.com>
2020-05-31 23:20:22 +0800

    Create 100-same-tree.js

---
+++ 100-same-tree.js
@@ -0,0 +1,44 @@
+/*
+给定两个二叉树，编写一个函数来检验它们是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+示例 1:
+
+输入:       1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+输出: true
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/same-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  return helper(p, q);
+};
+
+function helper(node1, node2) {
+  if (!node1 && !node2) return true;
+
+  if (!node1 || !node2) return false;
+
+  if (node1.val !== node2.val) return false;
+
+  return helper(node1.left, node2.left) && helper(node1.right, node2.right);
+}
